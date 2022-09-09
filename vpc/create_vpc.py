400b626b6cf7e6ccfcf8c7da1eb6aaded09e9f9f
BabatundeOye <teamsoldd@gmail.com>
2022-09-09 19:49:46 +0000

    updated line 6

--- vpc/create_vpc.py
+++ vpc/create_vpc.py
@@ -1,14 +1,15 @@
 #how to create vpc
 
-'''
+
 import boto3
 client=boto3.client("ec2")
-client.create_vpc(CidrBlock='10.0.0.0/16')
-'''
+response=client.create_vpc(CidrBlock='10.0.0.0/16')
+print(response)
+
 
 
 #checking for vpc id
-
+'''
 import boto3
 client=boto3.client("ec2")
 x=client.describe_vpcs()
@@ -16,3 +17,4 @@ no_of_vpcs=x["Vpcs"]
 
 for vpc in no_of_vpcs:
     print(vpc["VpcId"])
+'''
